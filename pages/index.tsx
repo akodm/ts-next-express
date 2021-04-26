023e6deeafb25990ffd530c5267ab90362b4955d
jojunmyeong <a8456452@naver.com>
2021-04-26 18:08:54 +0900

    test layout commit.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import ButtonComponent from '@Components/Button';
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+`;
 
 const Home = () => {
   return (
-    <div>
+    <Layout>
       <ButtonComponent />
-    </div>
+    </Layout>
   );
 }
 
